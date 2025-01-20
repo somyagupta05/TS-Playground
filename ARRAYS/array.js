4a2d340ce8c21802fdab0175b1d3bbba2e00bdf4
somyagupta05 <somyagupta052003@gmail.com>
2025-01-20 23:11:22 +0530

    Arrays

--- ARRAYS/array.js
+++ ARRAYS/array.js
@@ -1,12 +1,8 @@
+"use strict";
 // using square brackets
-
 // const numbers:number[]=[1,2,3,4,5,6]
-
 // using the Array constructor
-
 // const numbers1:number[]=new Array(1,2,3,4,5,6);
-
 // using the array. of method
-const names:string[]=Array.of("somya","gupta")
-
-console.log(names[1])
+const names = Array.of("somya", "gupta");
+console.log(names[1]);
