6745bdfec6a6e1b1352cdfe51c3994fda86eed11
somyagupta05 <somyagupta052003@gmail.com>
2025-01-20 23:06:22 +0530

    Arrays

---
+++ ARRAYS/array.ts
@@ -0,0 +1,11 @@
+// using square brackets
+
+const numbers:number[]=[1,2,3,4,5,6]
+
+// using the Array constructor
+
+const numbers1:number[]=new Array(1,2,3,4,5,6);
+
+// using the array. of method
+const names:string[]=Array.of("somya","gupta")
+
