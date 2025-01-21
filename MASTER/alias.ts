d3154e8be53d8f193ab48ee4448c22a7b7d66a33
somyagupta05 <somyagupta052003@gmail.com>
2025-01-21 22:58:50 +0530

    Alias

---
+++ MASTER/alias.ts
@@ -0,0 +1,35 @@
+// p of Person should be capital
+type Person={
+  name:string,
+  age:number,
+  isStudent:boolean,
+  address:{city:string;country:string}
+}
+
+const person2:Person=
+{
+
+  name:"somya",
+  age:22,
+  isStudent:true,
+  address:{
+    city:"Pune",
+    country:"India"
+  }
+
+}
+
+
+const person3:Person
+ = {
+
+
+  name:"somya",
+  age:22,
+  isStudent:true,
+  address:{
+    city:"Pune",
+    country:"India"
+  }
+
+}
