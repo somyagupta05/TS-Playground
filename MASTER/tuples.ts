f41d9e2356206a21fddd56e6ca81ad6f66886d77
somyagupta05 <somyagupta052003@gmail.com>
2025-01-23 16:48:10 +0530

    Tuples

--- MASTER/tuples.ts
+++ MASTER/tuples.ts
@@ -1,6 +1,8 @@
 // they allows you to store a fixed-size collection of elements of different types. they are similar to arrays, but with a key differences: the types of elements in a tuple are fixed and declared at the time of creation, whereas arrays can hold elements of the same type, and their size can vary.
 
-type PersonInfo=[string,number,boolean]
+// use when you are sure about data ki itna hi hoga ex name , age ....
+
+type PersonInfo=readonly[string,number,boolean]
 
 // function
 
