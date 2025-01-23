60845d0c1e6b3e0e71582b201b0090c775b5360a
somyagupta05 <somyagupta052003@gmail.com>
2025-01-23 16:33:21 +0530

    Enums

---
+++ MASTER/enums.js
@@ -0,0 +1,26 @@
+"use strict";
+// Enum in typescript are commonly used when you want to represent a set of related values and choose one value from multiple options. Enums provide a convenient way to define a set of named values and associate them with specific meanings.
+var Roles;
+(function (Roles) {
+    Roles["user"] = "user";
+    Roles["admin"] = "admin";
+})(Roles || (Roles = {}));
+const user1 = {
+    name: "somya",
+    email: "somya@gmail.com",
+    password: "asdf",
+    role: Roles.admin
+};
+const user2 = {
+    email: "gupta@gmail.com",
+    password: "asdf",
+    role: Roles.user
+};
+const isAdmin = (user) => {
+    const { name = "User", role } = user; // Default name to "User" if not provided
+    return role === Roles.admin
+        ? `${name} is allowed to edit the website`
+        : `${name} is not allowed to edit the website`;
+};
+console.log(isAdmin(user1));
+console.log(isAdmin(user2));
--- MASTER/enums.ts
+++ MASTER/enums.ts
@@ -1,1 +1,35 @@
 // Enum in typescript are commonly used when you want to represent a set of related values and choose one value from multiple options. Enums provide a convenient way to define a set of named values and associate them with specific meanings.
+
+
+enum Roles{
+  user="user",
+  admin="admin",
+}
+
+type LoginDetails={
+  name?:string;
+  email:string;
+  password:string;
+  role:Roles;
+}
+const user1:LoginDetails={
+  name:"somya",
+  email:"somya@gmail.com",
+  password:"asdf",
+  role:Roles.admin
+}
+
+const user2:LoginDetails={
+  email:"gupta@gmail.com",
+  password:"asdf",
+  role:Roles.user
+}
+
+const isAdmin = (user: LoginDetails): string => {
+  const { name = "User", role } = user; 
+  return role === Roles.admin 
+    ? `${name} is allowed to edit the website`
+    : `${name} is not allowed to edit the website`;
+}
+console.log(isAdmin(user1));
+console.log(isAdmin(user2));
