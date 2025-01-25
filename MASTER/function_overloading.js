cc04d5ffd6267bb187e6f6d4c211c73a3d4aa3ab
somyagupta05 <somyagupta052003@gmail.com>
2025-01-25 16:05:13 +0530

    function overloading

--- MASTER/function_overloading.js
+++ MASTER/function_overloading.js
@@ -1,6 +1,12 @@
 "use strict";
+// function add(a:number,b:number):number;
+// function add(a:string,b:string):string;
+// function add(a:any,b:any):any {
+//   return a+b;
+// }
 function add(a, b) {
-    return a + b;
+    console.log(typeof a);
+    console.log(typeof b);
 }
 const result1 = add(5, 10);
 const result2 = add("hello", "world");
--- MASTER/function_overloading.ts
+++ MASTER/function_overloading.ts
@@ -1,8 +1,12 @@
-function add(a:number,b:number):number;
-function add(a:string,b:string):string;
-function add(a:any,b:any):any {
-  return a+b;
-}
+// function add(a:number,b:number):number;
+// function add(a:string,b:string):string;
+// function add(a:any,b:any):any {
+//   return a+b;
+// }
 
-const result1:number=add(5,10)
-const result2:string=add("hello","world")
+function add<T>(a:T,b:T):void{
+console.log(typeof a);
+console.log(typeof b);
+}
+const result1:void=add<number>(5,10)
+const result2:void=add("hello","world")
