bca8f1762616d0cb68a74ba436f77c155baf1eba
somyagupta05 <somyagupta052003@gmail.com>
2025-01-25 16:07:39 +0530

    function overloading

--- MASTER/function_overloading.js
+++ MASTER/function_overloading.js
@@ -4,9 +4,17 @@
 // function add(a:any,b:any):any {
 //   return a+b;
 // }
+// .................
+//for both number or both string
+// function add<T>(a:T,b:T):void{
+// console.log(typeof a);
+// console.log(typeof b);
+// }
+// const result1:void=add<number>(5,10)
+// const result2:void=add("hello","world")
 function add(a, b) {
     console.log(typeof a);
     console.log(typeof b);
 }
-const result1 = add(5, 10);
+const result1 = add(5, "somya");
 const result2 = add("hello", "world");
--- MASTER/function_overloading.ts
+++ MASTER/function_overloading.ts
@@ -4,9 +4,18 @@
 //   return a+b;
 // }
 
-function add<T>(a:T,b:T):void{
-console.log(typeof a);
-console.log(typeof b);
-}
-const result1:void=add<number>(5,10)
-const result2:void=add("hello","world")
+// .................
+//for both number or both string
+// function add<T>(a:T,b:T):void{
+// console.log(typeof a);
+// console.log(typeof b);
+// }
+// const result1:void=add<number>(5,10)
+// const result2:void=add("hello","world")
+
+function add<T,U>(a:T,b:U):void{
+  console.log(typeof a);
+  console.log(typeof b);
+  }
+  const result1:void=add<number,string>(5,"somya")
+  const result2:void=add("hello","world")
