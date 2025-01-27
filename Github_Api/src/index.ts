e61b6b3e0467a53e834111fb07478acc2336c3b0
somyagupta05 <somyagupta052003@gmail.com>
2025-01-27 19:01:23 +0530

    github-api

--- Github_Api/src/index.ts
+++ Github_Api/src/index.ts
@@ -1,5 +1,5 @@
 const getUsername = document.querySelector("#user") as HTMLInputElement;
-const formSubmit = document.querySelector(".form") as HTMLFormElement;
+const formSubmit = document.querySelector("#form") as HTMLFormElement;
 const main_Container = document.querySelector(".main_container") as HTMLElement; // Fixed typo in class name
 
 interface UserData {
