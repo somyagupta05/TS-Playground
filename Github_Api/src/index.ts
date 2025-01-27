a63802ae580c6bacf7cc01c910451dd9ce6ee733
somyagupta05 <somyagupta052003@gmail.com>
2025-01-27 18:30:36 +0530

    github-api

---
+++ Github_Api/src/index.ts
@@ -0,0 +1,3 @@
+const getUsername=document.querySelector("#user") as HTMLInputElement;
+const formSubmit=document.querySelector(".form") as HTMLFormElement;
+const main_Container=document.querySelector(".main_conatiner") as HTMLElement
