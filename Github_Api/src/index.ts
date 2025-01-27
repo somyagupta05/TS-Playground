e9e062f0a0dea44c8353cdd191dd2466bd92b930
somyagupta05 <somyagupta052003@gmail.com>
2025-01-27 18:36:58 +0530

    github-api

--- Github_Api/src/index.ts
+++ Github_Api/src/index.ts
@@ -1,3 +1,20 @@
 const getUsername=document.querySelector("#user") as HTMLInputElement;
 const formSubmit=document.querySelector(".form") as HTMLFormElement;
-const main_Container=document.querySelector(".main_conatiner") as HTMLElement
+const main_Container=document.querySelector(".main_conatiner") as HTMLElement;
+
+interface UserData{
+  id:number;
+  login:string;
+  avatar_url:string;
+  location:string;
+  url:string
+}
+
+
+// reusable fun
+function myCustomeFetcher(url:string,options?:RequestInit):Promise<T> {}
+function fetchUserData(url:string){
+  myCustomeFetcher<UserData[]>(url,{});
+}
+// default fun call
+fetchUserData("https://api.github.com/users")
