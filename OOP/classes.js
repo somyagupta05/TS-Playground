9db4b183c5ded6c236fb357901465957f88ddf71
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 16:46:13 +0530

    oop

--- OOP/classes.js
+++ OOP/classes.js
@@ -1,13 +1,12 @@
+"use strict";
 // A class in terms  of oop is blueprint for creating objects
-
 // in typescript there is a convention to use pascalcase(also known as uppercase) for class name . this means that class anme should start with a capital letter and each subsequent word in the class anme should also beigin with a capital letter 
-
-
-class Persons{
-  name:string="somya";
-  age:number=22;
-  hobbies:string[]=["reading","painting"];
+class Persons {
+    constructor() {
+        this.name = "somya";
+        this.age = 22;
+        this.hobbies = ["reading", "painting"];
+    }
 }
-
-const person1:Persons=new Persons();
-console.log(person1.hobbies)
+const person1 = new Persons();
+console.log(person1.hobbies);
