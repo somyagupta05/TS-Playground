9135847128fc8d1038fee94d80d94b503dc0423f
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 17:14:22 +0530

    modifiers

---
+++ OOP/modifiers.ts
@@ -0,0 +1,5 @@
+  // parent class    child class    outside class 
+//public  yes               yes           yes
+//protected  yes            yes           no
+//private     yes           no            no
+
