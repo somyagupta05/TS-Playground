c02fd44c22aaeff6c283d251aa8e6cf9bee541ad
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 17:22:16 +0530

    modifiers

--- OOP/modifiers.js
+++ OOP/modifiers.js
@@ -22,11 +22,5 @@ class Student1 extends Person1 {
         return `${super.introduce()} I am in grade ${this.grade}.`;
     }
 }
-// Example usage
 const student1 = new Student1("Somya", 12, ["playing", "reading"], 7);
 console.log(student1.introduce()); // Accessible
-// console.log(student1.displayHobbies()); // Accessible
-// Examples of access
-// console.log(student1.grade); // Error: 'grade' is private
-// console.log(student1.age); // Error: 'age' is protected
-// console.log(student1.hobbies); // Error: 'hobbies' is private
