2e57632c8e7179d6e55460e38ae53a92ec0ce5e8
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 16:50:41 +0530

    constructor

--- OOP/classes.js
+++ OOP/classes.js
@@ -1,12 +1,21 @@
 "use strict";
 // A class in terms  of oop is blueprint for creating objects
 // in typescript there is a convention to use pascalcase(also known as uppercase) for class name . this means that class anme should start with a capital letter and each subsequent word in the class anme should also beigin with a capital letter 
+// class Persons{
+//   name:string="somya";
+//   age:number=22;
+//   hobbies:string[]=["reading","painting"];
+// }
+// const person1:Persons=new Persons();
+// console.log(person1.hobbies)
 class Persons {
-    constructor() {
-        this.name = "somya";
-        this.age = 22;
-        this.hobbies = ["reading", "painting"];
+    constructor(names, age, hobbies) {
+        this.name = names;
+        this.age = age;
+        this.hobbies = hobbies;
     }
 }
-const person1 = new Persons();
+const person1 = new Persons("somya", 12, ["playing"]);
+const person2 = new Persons("gupta", 12, ["playing", "reading"]);
 console.log(person1.hobbies);
+console.log(person2.hobbies);
--- OOP/classes.ts
+++ OOP/classes.ts
@@ -3,11 +3,30 @@
 // in typescript there is a convention to use pascalcase(also known as uppercase) for class name . this means that class anme should start with a capital letter and each subsequent word in the class anme should also beigin with a capital letter 
 
 
+// class Persons{
+//   name:string="somya";
+//   age:number=22;
+//   hobbies:string[]=["reading","painting"];
+// }
+
+// const person1:Persons=new Persons();
+// console.log(person1.hobbies)
+
 class Persons{
-  name:string="somya";
-  age:number=22;
-  hobbies:string[]=["reading","painting"];
+  name:string;
+  age:number;
+  hobbies:string[];
+
+  constructor(names:string,age:number,hobbies:string[]){
+    this.name=names;
+    this.age=age;
+    this.hobbies=hobbies;
+
+  }
 }
 
-const person1:Persons=new Persons();
+const person1:Persons=new Persons("somya",12,["playing"]);
+
+const person2:Persons=new Persons("gupta",12,["playing","reading"]);
 console.log(person1.hobbies)
+console.log(person2.hobbies)
