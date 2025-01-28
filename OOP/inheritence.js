c54656c11ac92013fb4c96e14618a97da6d4b5b8
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 17:07:29 +0530

    inheritence

--- OOP/inheritence.js
+++ OOP/inheritence.js
@@ -1,5 +1,6 @@
 "use strict";
 // inheritence allows a class to reuse functionality of an existing class without rewriting it.
+// in ts the super keyword is used in the context of class inheritence. it allows a subclass (also known as a derived class) to call methods or access properties of its superclass (also known as a base class). This is particularly useful when you want to extend  the behaviour of a parent class while still leveraging its existing functionality.
 class Person {
     constructor(name, age, hobbies) {
         this.name = name;
@@ -11,10 +12,17 @@ class Person {
     }
 }
 class Student extends Person {
+    constructor(name, age, hobbies, grade) {
+        super(name, age, hobbies); // Call the parent class constructor
+        this.grade = grade; // Assign the additional property
+    }
+    introduce() {
+        return `Hi, I am ${this.name} and I'm ${this.age} years old. I am in grade ${this.grade} I love ${this.hobbies.join(", ")}.`;
+    }
 }
 class Teachers {
 }
 const person3 = new Person("Somya", 12, ["playing"]);
-const students1 = new Student("Gupta", 12, ["playing", "reading"]);
+const students1 = new Student("Gupta", 12, ["playing", "reading"], 9);
 console.log(person3.introduce());
 console.log(students1.introduce());
--- OOP/inheritence.ts
+++ OOP/inheritence.ts
@@ -1,5 +1,8 @@
 // inheritence allows a class to reuse functionality of an existing class without rewriting it.
 
+
+// in ts the super keyword is used in the context of class inheritence. it allows a subclass (also known as a derived class) to call methods or access properties of its superclass (also known as a base class). This is particularly useful when you want to extend  the behaviour of a parent class while still leveraging its existing functionality.
+
 class Person {
   name: string;
   age: number;
@@ -17,12 +20,21 @@ class Person {
   }
 }
 
-class Student extends Person{}
+class Student extends Person {
+  grade: number;
+
+  constructor(name: string, age: number, hobbies: string[], grade: number) {
+    super(name, age, hobbies); // Call the parent class constructor
+    this.grade = grade; // Assign the additional property
+  }
+  introduce(): string {
+    return `Hi, I am ${this.name} and I'm ${this.age} years old. I am in grade ${this.grade} I love ${this.hobbies.join(", ")}.`;
+  }}
 
 class Teachers{}
 
 const person3: Person = new Person("Somya", 12, ["playing"]);
-const students1:Student = new Student("Gupta", 12, ["playing", "reading"]);
+const students1:Student = new Student("Gupta", 12, ["playing", "reading"],9);
 
 console.log(person3.introduce());
 console.log(students1.introduce());
