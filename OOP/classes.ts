c18f95b95871b87215327a737bd2ef08205864d8
somyagupta05 <somyagupta052003@gmail.com>
2025-01-28 16:42:24 +0530

    oop

---
+++ OOP/classes.ts
@@ -0,0 +1,3 @@
+// A class in terms  of oop is blueprint for creating objects
+
+// in typescript there is a convention to use pascalcase(also known as uppercase) for class name . this means that class anme should start with a capital letter and each subsequent word in the class anme should also beigin with a capital letter 
