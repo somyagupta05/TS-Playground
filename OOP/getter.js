18aea36d494d0abd28ab273805e3a4ee2326ede9
somyagupta05 <somyagupta052003@gmail.com>
2025-01-29 19:25:10 +0530

    getter and setter

--- OOP/getter.js
+++ OOP/getter.js
@@ -1,17 +1,26 @@
 "use strict";
 // In typescript classes, you can use getter and setter methods to control the access and modification of class properties. Getter methods allow you to retrieve the value of property, while setter allow you to set the value of a property with additional logic or validation. 
 class Personss1 {
-    constructor(name, age, hobbies) {
+    constructor(name, hobbies) {
         this.name = name;
-        this.age = age;
         this.hobbies = hobbies;
-        if (this.age > 150 || this.age < 0) {
+    }
+    set age(age) {
+        if (age > 150 || age < 0) {
             throw new Error("age is not valid");
         }
+        this._age = age;
+    }
+    get age() {
+        if (this._age === undefined) {
+            throw new Error("age is not defined");
+        }
+        return this._age;
     }
     introduce() {
-        return `Hi, I am ${this.name} and I'm ${this.age} years old. I love ${this.hobbies.join(", ")}.`;
+        return `Hi, I am ${this.name} and I'm ${this._age} years old. I love ${this.hobbies.join(", ")}.`;
     }
 }
-const person30 = new Personss1("Somya", 212, ["playing"]);
+const person30 = new Personss1("Somya", ["playing"]);
+person30.age = 526;
 console.log(person30.introduce());
--- OOP/getter.ts
+++ OOP/getter.ts
@@ -1,28 +1,39 @@
 // In typescript classes, you can use getter and setter methods to control the access and modification of class properties. Getter methods allow you to retrieve the value of property, while setter allow you to set the value of a property with additional logic or validation. 
 
 class Personss1 {
+  private _age: number| undefined;
   constructor(
     public name: string,
-    public age: number,
+  
     protected hobbies: string[],
   
-  )
-  
-    {if(this.age>150 || this.age<0){
+  ){}
+  public set age(age:number){
+    if(age>150 || age<0){
       throw new Error("age is not valid")
-    }}
+    }
+    this._age=age;
+  }
+  public get age():number{
+    if(this._age===undefined)
+    {
+      throw new Error("age is not defined")
+    }
+    return this._age;
+  }
+   
   
     introduce(): string {
-      return `Hi, I am ${this.name} and I'm ${this.age} years old. I love ${this.hobbies.join(", ")}.`;
+      return `Hi, I am ${this.name} and I'm ${this._age} years old. I love ${this.hobbies.join(", ")}.`;
     }
   }
   
   
   
   
-  const person30: Personss1 = new Personss1("Somya", 212, ["playing"]);
+  const person30: Personss1 = new Personss1("Somya",  ["playing"]);
 
-  
+  person30.age=56;
   console.log(person30.introduce());
 
   
