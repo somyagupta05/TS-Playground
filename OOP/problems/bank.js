d5ef270f3a7812e6d037997b7a259d15c9ff8f4b
somyagupta05 <somyagupta052003@gmail.com>
2025-01-29 19:35:42 +0530

    bank account

---
+++ OOP/problems/bank.js
@@ -0,0 +1,25 @@
+"use strict";
+// Bank account balance
+// create a typescript class bankaccount with a private property_balance intialized to 0.
+// implement a getter method balance that return current balance.
+// implement a setter method balance that updates the balance if the new value is non negative otherwise, log an error message.
+// Instatiate an object of the bankaccount class.
+// use the setter to set thr balance to 1000 and use the getter to display the uodated balance.
+// try setting a negative balance using the setter. what output do you expect.
+class BankAccount {
+    constructor() {
+        this._balance = 0;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(newBalance) {
+        if (newBalance < 0) {
+            throw new Error("invalid balance");
+        }
+        this._balance = newBalance;
+    }
+}
+const account = new BankAccount();
+account.balance = -10;
+console.log(account.balance);
---
+++ OOP/problems/bank.ts
@@ -0,0 +1,25 @@
+// Bank account balance
+// create a typescript class bankaccount with a private property_balance intialized to 0.
+// implement a getter method balance that return current balance.
+// implement a setter method balance that updates the balance if the new value is non negative otherwise, log an error message.
+// Instatiate an object of the bankaccount class.
+// use the setter to set thr balance to 1000 and use the getter to display the uodated balance.
+// try setting a negative balance using the setter. what output do you expect.
+
+class BankAccount{
+  private _balance:number=0;
+
+  public get balance():number{
+    return this._balance;
+  }
+  public set balance(newBalance:number){
+    if(newBalance<0)
+    {
+      throw new Error("invalid balance");
+    }
+    this._balance=newBalance;
+  }
+}
+const account=new BankAccount();
+account.balance=-10;
+console.log(account.balance)
