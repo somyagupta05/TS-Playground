716676966c4e650150936ed674350d230b3d0559
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 16:13:52 +0530

    diff in types and interface

---
+++ OOP/interface_type.js
@@ -0,0 +1,9 @@
+"use strict";
+// use custom types when you need unions, intersections, or mapped types
+// use interfaces when defining object shapes or classes that adhere to a contract.
+const bioData = {
+    name: "somya",
+    city: "pune",
+    state: "mp"
+};
+console.log(bioData);
--- OOP/interface_type.ts
+++ OOP/interface_type.ts
@@ -3,3 +3,22 @@
 
 // interfaces can extend other interfaces to inherit their members.
 // custom types can use sunions and intersections for more complex type compositions.
+
+type Stud={
+  name:string;
+  age:number;
+};
+type StudAddr={
+  city:string;
+  state:string;
+};
+
+type Data=Stud | StudAddr;
+
+const bioData:Stud | StudAddr ={
+  name:"somya",
+  city:"pune",
+  state:"mp"
+}
+
+console.log(bioData)
