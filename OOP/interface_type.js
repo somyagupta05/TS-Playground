f5d84818a772114eb29cbc990b44096417e7f7dc
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 16:18:22 +0530

    diff in types and interface

--- OOP/interface_type.js
+++ OOP/interface_type.js
@@ -1,9 +1,12 @@
 "use strict";
 // use custom types when you need unions, intersections, or mapped types
 // use interfaces when defining object shapes or classes that adhere to a contract.
+;
+;
 const bioData = {
     name: "somya",
     city: "pune",
-    state: "mp"
+    state: "mp",
+    age: 22,
 };
 console.log(bioData);
