8570823237e49cc40b0287cee8c5824af4968be1
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 15:52:49 +0530

    abstract class

---
+++ OOP/abstract.ts
@@ -0,0 +1,4 @@
+// abstract classes provide a way to define common properties and methods that multiple derived classes can share. This process code reuse and helps establish a common interface for related class
+
+// abstract class focus on class inheritance and sharing common functionality
+// whereas the useContext hook in react on managing global state and allowing components to consume that state.
