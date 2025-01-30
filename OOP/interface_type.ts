821d71fdc49cfb82c97d9bea6306d06c1e341610
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 16:11:04 +0530

    diff in types and interface

---
+++ OOP/interface_type.ts
@@ -0,0 +1,5 @@
+// use custom types when you need unions, intersections, or mapped types
+// use interfaces when defining object shapes or classes that adhere to a contract.
+
+// interfaces can extend other interfaces to inherit their members.
+// custom types can use sunions and intersections for more complex type compositions.
