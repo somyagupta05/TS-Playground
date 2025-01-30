ccf1e21523953a1caf96abe39ba149e78f8d85cf
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 16:01:36 +0530

    abstract class

--- OOP/abstract.ts
+++ OOP/abstract.ts
@@ -2,3 +2,26 @@
 
 // abstract class focus on class inheritance and sharing common functionality
 // whereas the useContext hook in react on managing global state and allowing components to consume that state.
+
+abstract class Shape{
+  constructor(protected color:string){}
+  abstract calculateArea():number;
+  abstract displayArea:()=>void;
+} 
+
+
+class Circle extends Shape{
+  constructor(protected color:string, protected radius:number) {
+    super(color);
+  }
+  public calculateArea():number{
+    return Math.PI*this.radius*this.radius
+  }
+  displayArea: () => void=(): void=>{
+  console.log(`this is ${this.color} circle with radius ${this.radius}`)
+  };
+}
+
+const circle =new Circle("Red",5);
+console.log(circle.calculateArea());
+circle.displayArea;
