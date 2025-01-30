18399ed91f21d70b5211c0a824ff8aefe3146a64
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 15:47:08 +0530

    static

--- OOP/static.ts
+++ OOP/static.ts
@@ -1,7 +1,10 @@
 // in Typescript static method and properties belong to class itself rather than to instances of the class. by making methods and properties static, we can access directly from the class without needing to create an instance of the class. This is useful for utility functions or properties that dony rely on instance-specific data.
 
-class Pperson{
- constructor( public name:string) {}
-}
 
-const user1=new Pperson("somya")
+
+// Math operations utility - creating a utility class to perform various mathematical operations
+
+class MathOperations{
+  public static PI: number=Math.PI;
+}
+console.log(MathOperations.PI)
