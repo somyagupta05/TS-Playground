91cf5a315355195bb4dda16dbd9269e5dc68b871
somyagupta05 <somyagupta052003@gmail.com>
2025-01-30 16:25:27 +0530

    type safety

---
+++ OOP/typesafety.ts
@@ -0,0 +1,1 @@
+//  a typeof guard in typescript lets you narrow down the type of a variable based on its runtime value. In typescript , type narrowing allows you to write type-safe code by ensuring you only operate on the correct type under certian circumstances. This is particularly useful with unions types and generic types.
